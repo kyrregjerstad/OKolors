41c6be3287e60add2955dbcdb047c364df5c721d
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-05-29 20:27:22 +0200

    add todo list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,34 @@
 	import AddNewColorButton from "$lib/components/buttons/AddNewColorButton.svelte";
 	import ColorRange from "$lib/components/color-range/ColorRange.svelte";
 	import { colorStore } from "$stores/colorStore";
+
+	/* 
+	
+	TODO: 
+	- Add a way to remove colors
+	- Add a way to change the order of colors
+
+	- Check if device is p3 compatible
+	- Check if screen is p3 compatible?
+
+	- Add better color picker
+
+	- Add copy button to code block
+	- Add option to toggle color name comments in code block
+
+	- Add presets
+
+	- Add login?
+	- Add option to save your own presets?
+	- Add option to share presets
+
+	- Highlight that the center color is the one that is being changed
+
+
+	- fix bug where the codeblock css is not being updated from the store
+
+	
+	*/
 </script>
 
 <h1>SvelteKit Default Template</h1>
