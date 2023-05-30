60b15b00c82644e5c99ccd02b9d0641e975a3e4b
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-05-30 15:40:48 +0200

    add skeleton UI

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
+	// Your selected Skeleton theme:
+	import "@skeletonlabs/skeleton/themes/theme-skeleton.css";
+
+	// This contains the bulk of Skeletons required styles:
+	// NOTE: this will be renamed skeleton.css in the v2.x release.
+	import "@skeletonlabs/skeleton/styles/skeleton.css";
+
+	// Finally, your application's global stylesheet (sometimes labeled 'app.css')
+	import "../app.postcss";
+
 	import * as config from "$config";
 	import MetaTags from "$lib/components/SEO/MetaTags.svelte";
-	import "$styles/app.css";
+	// import "$styles/app.css";
 </script>
 
 <svelte:head>
