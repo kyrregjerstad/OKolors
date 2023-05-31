25bcecde434abc68b401f8aa1e5052797abb8757
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-05-31 11:30:02 +0200

    🔧 refactor(ColorSpaceCompatibility): move deviceColorSpace logic from +page.svelte to component 💄 style(ColorSpaceCompatibility): improve styling and add conditional classes for p3 and not p3 color spaces

--- src/lib/components/ColorSpaceCompatibility.svelte
+++ src/lib/components/ColorSpaceCompatibility.svelte
@@ -1,9 +1,27 @@
 <script lang="ts">
-	export let deviceColorSpace: string | null = null;
+	import { onMount } from "svelte";
+	let deviceColorSpace: string | null = null;
+	onMount(async () => {
+		deviceColorSpace = window.matchMedia("(color-gamut: p3)").matches ? "p3" : "not p3";
+	});
 </script>
 
-<div class="card p-5 text-center my-5 variant-filled mx-auto flex w-fit">
-	<p>Your device's color space: {deviceColorSpace || "please wait..."}</p>
+<!-- 
+	
+
+	 -->
+<div
+	class="card variant-filled mx-auto my-10 flex w-fit p-5 text-center !text-white"
+	class:!bg-success-500={deviceColorSpace === "p3"}
+	class:!text-black={deviceColorSpace === "p3"}
+	class:!bg-error-500={deviceColorSpace === "not p3"}
+>
+	<p>
+		Your device's color space:
+		<span class="font-bold">
+			{deviceColorSpace || "please wait..."}
+		</span>
+	</p>
 </div>
 
 <style>
