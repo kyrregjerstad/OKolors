ef8463c00f17375c0dab77bd64896861c7755023
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-07-24 10:35:16 +0200

    update title

--- src/lib/config/config.ts
+++ src/lib/config/config.ts
@@ -1,6 +1,6 @@
 import { dev } from "$app/environment";
 
-export const title = "OKolor";
+export const title = "OKolors";
 export const description = "";
 export const url = dev ? "http://localhost:5177/" : "url";
 export const author = "Kyrre Gjerstad";
